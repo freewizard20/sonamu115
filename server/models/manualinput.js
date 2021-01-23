216bcd23dbfade28c42e9b376d6c68945a041de0
freewizard20 <gseongkyu79@gmail.com>
2021-01-23 20:12:56 +0900

    d

--- server/models/manualinput.js
+++ server/models/manualinput.js
@@ -443,6 +443,7 @@ function countThumbnails(){
 				console.log('Number of thumbnails : ' + files.length);
 				let test = 0;
 				for(let i = 0 ; i < files.length ; i++){
+					console.log(getFilesizeInBytes(dir+'/'+files[i]));
 					if(getFilesizeInBytes(dir+'/'+files[i])===0){
 						console.log(files[i] + ' corrupt');
 						test++;
